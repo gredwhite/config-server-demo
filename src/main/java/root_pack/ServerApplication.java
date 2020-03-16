94798b97ce284f7ef95a8706ac3327bba6893b19
Nikolay Tkachev <nikolay.tkachev@bouncex.com>
2020-03-16 11:42:04 +0300

    corrected library versions added support for app engine renamed main class

--- src/main/java/root_pack/ServerApplication.java
+++ src/main/java/root_pack/ServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class MyApplication {
+public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(ServerApplication.class, args);
     }
 }
